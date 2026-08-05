916d2c267bdddc4f629df7dee7d5fd47f640278b
Venkatanaga Vaishnav Gadamsetty <gvnagavaishnav@gmail.com>
2026-08-06 02:18:01 +0530

    fix: downgrade next-themes and use ThemeProviderProps to resolve hydration error related to script tags injected by next-themes

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,11 +1,11 @@
 "use client"
 
 import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from "next-themes"
 
 export function ThemeProvider({
   children,
   ...props
-}: React.ComponentProps<typeof NextThemesProvider>) {
+}: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
